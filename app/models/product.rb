13fd0c32bcbbf8559367d9e76df79f408e4118e2
mireie <michael.reiersgaard@gmail.com>
2021-08-06 16:00:09 -0700

    add scopes

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,7 +5,17 @@ class Product < ApplicationRecord
   end
   validates_length_of :name, maximum: 100
   before_save(:titleize_product)
+  scope :country, ->(country) { where(country_of_origin: country) }
+  scope :most_reviewed, -> {
+          (select("products.id, products.name, count(reviews.id) as reviews_count")
+            .joins(:reviews)
+            .group("products.id")
+            .order("reviews_count")
+            .limit(1))
+        }
+  scope :threecent, -> { order(created_at: :desc).limit(3)}
   private
+
   def titleize_product
     self.name = self.name.titleize
   end
