1f7f2f5cae9e31058a66cea956a9b260e23ca1eb
mireie <michael.reiersgaard@gmail.com>
2021-08-06 12:34:26 -0700

    update show review

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,15 +7,31 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 Product.destroy_all
+Review.destroy_all
 
 50.times do
-  Product.create!( 
-    name: Faker::Hipster.words(number: 3, spaces_allowed:true).join(" "),
+  Product.create!(
+    name: Faker::Hipster.words(number: 3, spaces_allowed: true).join(" "),
     description: Faker::Hipster.paragraph,
-    cost: Faker::Number.decimal(r_digits:2),
-    price: Faker::Number.decimal(r_digits:2),
-    country_of_origin: Faker::Address.country
+    cost: Faker::Number.decimal(r_digits: 2),
+    price: Faker::Number.decimal(r_digits: 2),
+    country_of_origin: Faker::Address.country,
   )
 end
 
-p "Created #{Product.count} products"
+p "Created #{Product.count} products."
+
+@products = Product.all
+
+@products.each do |product|
+  rand(0..5).times do
+    Review.create!(
+      author: Faker::Name.name,
+      rating: rand(1..5),
+      content_body: Faker::Hipster.sentences(number: 3).join(" "),
+      product_id: product.id,
+    )
+  end
+end
+
+p "Created #{Review.count} Reviews for #{Product.count} products."
