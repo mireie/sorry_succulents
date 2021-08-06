4a196f1f754e0da6dcee31f774463e206f0b7793
mireie <michael.reiersgaard@gmail.com>
2021-08-06 08:55:59 -0700

    add first migration

---
+++ db/migrate/20210806155142_create_products.rb
@@ -0,0 +1,13 @@
+class CreateProducts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :products do |t|
+    t.string :name
+    t.integer :cost
+    t.integer :price
+    t.text :description
+    t.string :country_of_origin
+
+    t.timestamps()
+    end
+  end
+end
