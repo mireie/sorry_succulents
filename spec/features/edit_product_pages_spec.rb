8892d4867800b788903429b16318ed77ebf2ab6e
mireie <michael.reiersgaard@gmail.com>
2021-08-06 21:44:57 -0700

    simplecov tests

---
+++ spec/features/edit_product_pages_spec.rb
@@ -0,0 +1,37 @@
+require "rails_helper"
+
+describe "the edit product process" do
+  before(:each) do
+    visit products_path
+    click_link "Create new product"
+    fill_in "Name", :with => "Pokey Friend"
+    fill_in "Cost", :with => 13
+    fill_in "Price", :with => 14
+    fill_in "Description", :with => "This needs to be fairly long so I will add a couple more words and we should be there."
+    fill_in "Country of origin", :with => "United States of America"
+    click_on "Create Product"
+    within ".main-content" do
+      click_on "Pokey Friend"
+    end
+    click_on "Edit"
+  end
+
+  it "edits a product" do
+    fill_in "Name", :with => "Super Pokey Friend"
+    click_on "Update Product"
+    expect(page).to have_content "Super"
+  end
+
+  it "gives an error when no name is entered" do
+    fill_in "Name", :with => ""
+    click_on "Update Product"
+    expect(page).to have_content "Name can't be blank"
+  end
+
+  it "deletes a product" do
+    visit products_path
+    within ".main-content" do
+      click_on "Pokey Friend"
+    end
+  end
+end
---
+++ spec/features/show_product_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+describe "visit pages" do
+  it "shows a product when clicked from the product" do
+    visit products_path
+    click_link 'Create new product'
+    fill_in 'Name', :with => 'Pokey Friend'
+    fill_in 'Cost', :with => 13
+    fill_in 'Price', :with => 14
+    fill_in 'Description', :with => "This needs to be fairly long so I will add a couple more words and we should be there."
+    fill_in 'Country of origin', :with => "United States of America"
+    click_on 'Create Product'
+    within '.main-content' do
+      click_on 'Pokey Friend'
+    end
+      expect(page).to have_content 'fairly long'
+  end
+
+end
