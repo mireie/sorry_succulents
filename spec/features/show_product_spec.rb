aa23a96933f60861703ca2614ee330da46d59d35
mireie <michael.reiersgaard@gmail.com>
2021-08-13 15:25:54 -0700

    add admin delete test

--- spec/features/show_product_spec.rb
+++ spec/features/show_product_spec.rb
@@ -8,10 +8,29 @@ describe "visit pages" do
       :description => "This needs to be fairly long so I will add a couple more words and we should be there.",
       :country_of_origin => "United States of America",
     )
-    visit ('/')
+    visit ("/")
     within ".main-content" do
       click_on "Pokey Friend"
     end
     expect(page).to have_content "fairly long"
+  end
+  it "should allow an admin to delete" do
+    user = User.create(:email => "test@example.com", :password => "password-test", admin: true)
+    visit new_user_session_url
+    fill_in "Email", :with => user.email
+    fill_in "Password", :with => "password-test"
+    click_on "Log in"
+    product = Product.create(
+      :name => "Pokey Friend",
+      :cost => 13, :price => 14,
+      :description => "This needs to be fairly long so I will add a couple more words and we should be there.",
+      :country_of_origin => "United States of America",
+    )
+    visit ("/")
+    within ".main-content" do
+      click_on "Pokey Friend"
+    end
+    click_on "Delete product"
+    expect(page).to have_content "Product successfully deleted!"
   end
 end
