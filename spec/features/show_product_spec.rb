ee0ab9ca32fa3a87354dc45c65a991ddec91c19b
mireie <michael.reiersgaard@gmail.com>
2021-08-13 15:00:24 -0700

    add user model spec

--- spec/features/show_product_spec.rb
+++ spec/features/show_product_spec.rb
@@ -1,25 +1,17 @@
-require 'rails_helper'
+require "rails_helper"
 
 describe "visit pages" do
   it "shows a product when clicked from the product" do
-      user = User.create(:email => 'test@example.com', :password => 'password-test', admin: true)
-      visit new_user_session_url
-      fill_in 'Email', :with => user.email
-      fill_in 'Password', :with => 'password-test'
-      click_on 'Log in'
-      visit new_product_path
-
-
-    fill_in 'Name', :with => 'Pokey Friend'
-    fill_in 'Cost', :with => 13
-    fill_in 'Price', :with => 14
-    fill_in 'Description', :with => "This needs to be fairly long so I will add a couple more words and we should be there."
-    fill_in 'Country of origin', :with => "United States of America"
-    click_on 'Create Product'
-    within '.main-content' do
-      click_on 'Pokey Friend'
+    product = Product.create(
+      :name => "Pokey Friend",
+      :cost => 13, :price => 14,
+      :description => "This needs to be fairly long so I will add a couple more words and we should be there.",
+      :country_of_origin => "United States of America",
+    )
+    visit ('/')
+    within ".main-content" do
+      click_on "Pokey Friend"
     end
-      expect(page).to have_content 'fairly long'
+    expect(page).to have_content "fairly long"
   end
-
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,7 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  [:email, :password].each do |field|
+    it { should validate_presence_of(field) }
+  end
 end
