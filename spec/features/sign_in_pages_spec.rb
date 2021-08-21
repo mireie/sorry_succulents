93ad8e7305db7adad68dc74f9173469f3c50889e
mireie <michael.reiersgaard@gmail.com>
2021-08-21 13:51:36 -0700

    add sign in specs

---
+++ spec/features/sign_in_pages_spec.rb
@@ -0,0 +1,18 @@
+require "rails_helper"
+
+describe "the sign in proccess" do
+  before(:each) do
+    User.destroy_all
+    User.create!({ :email => "customer@example.com", :password => "password", :admin => false })
+  end
+
+  it "signs in a user" do
+    visit new_user_session_url
+    fill_in "Email", :with => "customer@example.com"
+    fill_in "Password", :with => "password"
+    within "#new_user" do
+      click_on "Log in"
+    end
+    expect(page).to have_content "Signed in successfully."
+  end
+end
